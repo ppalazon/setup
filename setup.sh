9524bb29a027588c22c2bd9ecaa526d167222620
Pablo Palazón <pablo.palazon@gmail.com>
2013-07-30 09:20:28 +0200

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -2,19 +2,21 @@
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 # for headless setup.
 
-workdir=realpath ./
+workdir=`realpath ./`
+
+# Update operating system dependencies
+sudo apt-get update && sudo apt-get upgrade
 
 # Install development dependencies
-sudo apt-get install -y git
-sudo apt-get install -y curl
-sudo apt-get install -y pip
-sudo apt-get install -y ntp
-sudo apt-get install -y build-essential
+sudo apt-get install -y git curl pip ntp build-essential rlwrap
 
 # Install bash-it
-if[ -d ~/.bash_it ]; then
+if [ ! -d ~/.bash_it ]; then
     git clone http://github.com/revans/bash-it.git ~/.bash_it
     ~/.bash_it/install.sh
+    ln -s ${workdir}/bash-it/aliases/custom.aliases.bash ~/.bash_it/aliases/
+    ln -s ${workdir}/bash-it/lib/custom.bash ~/.bash_it/lib/
+    ln -s ${workdir}/bash-it/plugins/custom.plugins.bash ~/.bash_it/plugins/
     echo 'You must re-login, or check your bash terminal as login terminal'
 fi
 
@@ -22,12 +24,6 @@ fi
 sudo pip install nodeenv
 
 # Install rvm
-curl -L https://get.rvm.io | bash -s stable
-
-# Install rlwrap to provide libreadline features with node
-# See: http://nodejs.org/api/repl.html#repl_repl
-sudo apt-get install -y rlwrap
-
-ln -s ${workdir}/bash-it/aliases/custom.aliases.bash ../.bash_it/aliases/
-ln -s ${workdir}/bash-it/lib/custom.bash ../.bash_it/lib/
-ln -s ${workdir}/bash-it/plugins/custom.plugins.bash ../.bash_it/plugins/
+if [ ! -d ~/.rvm ]; then
+    curl -L https://get.rvm.io | bash -s stable
+fi
